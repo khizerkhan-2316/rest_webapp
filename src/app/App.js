2270347b78a56ffe15a677eaa1ebc425362db241
khizerkhan-2316 <khizer759@gmail.com>
2021-12-03 22:08:53 +0100

    added navigationbar

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import NavigationBar from "../components/navigationbar.js";
+
+function App() {
+  return (
+    <>
+      <NavigationBar />
+    </>
+  );
+}
+
+export default App;
